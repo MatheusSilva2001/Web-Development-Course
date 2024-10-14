9f8028366858f20d0bc227311e7fbb6b0f849597
Matheus Rian <silva.matheus200107@gmail.com>
2024-10-14 20:25:58 -0300

    add class 01 data types

---
+++ 2-JavaScript/class01-data-types/script.js
@@ -0,0 +1,27 @@
+let myName = "Matheus" // string
+const cpf = "123.123.123-12"; // string
+let age = 17; // number
+let isAdmin = true; // boolean
+
+
+myName = "Matheus Rian"
+console.log(myName);
+console.log(cpf);
+console.log(age + 1); // type cohesion
+console.log(isAdmin);
+document.write("Bem vindo ao sistema, " + myName);
+console.log(Number("2") + 3); // type conversion    
+
+// template literals (string)
+console.log(`Meu nome é ${myName} e tenho ${age} anos`);
+
+
+
+
+
+console.log(typeof myName);
+console.log(typeof cpf);
+console.log(typeof age)
+console.log(typeof isAdmin);
+
+
---
+++ 2-JavaScript/class01-data-types/structural.js
@@ -0,0 +1,51 @@
+const user = {
+  name: "Goku",
+  age: 51,
+  rule: "Saiyajin",
+  Saiyajin1: true,
+};
+const user2 = {
+  name: "Gohan",
+  age: 15,
+  rule: "Saiyajin",
+  Saiyajin1: true,
+};
+
+console.log(user);
+console.log(user.name);
+console.log(user.age);
+console.log(user.rule);
+console.log(user2);
+console.log(user2.name);
+console.log(user2.age);
+console.log(user2.rule);
+
+// array (vector)
+
+const animes = [
+  "Os Cavaleiros do Zodíacos",
+  "Dragon Ball",
+  "Naruto",
+  "ThunderCats",
+  "Heeman",
+];
+
+console.log(animes); 
+console.log(animes[1]); 
+
+const pokemon = [
+    
+       { name: "Charmander", level: 6 },
+       { name: "Bulbasaur", level: 7 },
+       { name: "Squirtle", level: 4 }
+    
+  ];
+
+  console.log(pokemon);
+  console.log(pokemon[0].name);
+  console.log(pokemon[0].level);
+  console.log(pokemon[1].name);
+  console.log(pokemon[1].level);
+  console.log(pokemon[2].name);
+  console.log(pokemon[2].level);
+  
