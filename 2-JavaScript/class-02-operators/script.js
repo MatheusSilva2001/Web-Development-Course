0ddb81898c4d6bdcec1f3874b00b1407ce8a1e1b
Matheus Rian <silva.matheus200107@gmail.com>
2024-10-15 20:21:49 -0300

    add class 02 operators

---
+++ 2-JavaScript/class-02-operators/script.js
@@ -0,0 +1,45 @@
+let num1 = 2
+let num2 = 3
+
+
+console.log(num1 + num2); // soma
+console.log(num1 - num2); // subtração
+console.log(num1 * num2); // multiplicação 
+console.log(num1 / num2); // divisão
+console.log(num1 ** num2); // potência 
+console.log(81 ** (1 / 2)); // raiz
+
+// assigment
+
+num1 = 10;
+num2 = num2 + 2;
+num1 = num1 - 6;
+
+num1 += 4
+num2 -=2
+console.log(num1);
+console.log(num2);
+
+// comparison
+
+console.log(10 == 10);
+console.log(10 != 10);
+console.log(2 < 1);
+console.log(2 > 1);
+console.log(2 >= 2);
+console.log(2 >= 2);
+console.log(20 == 2);
+
+console.log(20 == "20");
+console.log(20 === "20");
+
+// logical (&& ||)
+const card = false;
+const credit = true;
+const cash = false;
+
+
+
+
+
+
