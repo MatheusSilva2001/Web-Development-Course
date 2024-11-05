ba56b6abb7e735d503441ee6ccee4374e118d8c6
Matheus Rian <silva.matheus200107@gmail.com>
2024-11-05 20:38:00 -0300

    add array map example

--- 2-JavaScript/class-06-loops/script.js
+++ 2-JavaScript/class-06-loops/script.js
@@ -11,30 +11,44 @@ while (count < 100) {
 
   if (count % 2 == 0) {
     console.log(count);
-    
   }
 }
 
-for (let count = 0; count <= 10; count ++ ) {
-    console.log(count);
-    
+for (let count = 0; count <= 10; count++) {
+  console.log(count);
 }
 
 const students = [
-    { name: "Pelé" , n1: 7, n2: 8},
-    { name: "Vini Jr" , n1: 8, n2: 6},
-    { name: "Mané Garrincha" , n1: 5, n2: 6},
-]
+  { name: "Pelé", n1: 7, n2: 8 },
+  { name: "Vini Jr", n1: 8, n2: 6 },
+  { name: "Mané Garrincha", n1: 5, n2: 6 },
+];
 
 for (let i = 0; i < students.length; i++) {
-    const avg = (students[i].n1 + students[i].n2) / 2;
+  const avg = (students[i].n1 + students[i].n2) / 2;
 
-    if (avg >= 7) {
-        console.log (`${students[i].name} aprovado por média ${avg}`);   
-    }
-    else {
-        console.log (`${students[i].name} reprovado por média ${avg}`);   
-    }
+  if (avg >= 7) {
+    console.log(`${students[i].name} aprovado por média ${avg}`);
+  } else {
+    console.log(`${students[i].name} reprovado por média ${avg}`);
+  }
 }
 
 console.log("End");
+
+// desafio
+
+const numbers = [];
+
+while (numbers.length < 6) {
+  const draw = Math.ceil(Math.random() * 60);
+  const numberFormatted = String(draw).padStart(2, 0);
+
+  if (numbers.includes(numberFormatted)) {
+    continue;
+  } else {
+    numbers.push(numberFormatted);
+  }
+}
+
+console.log(numbers.sort((a, b) => a - b).join("-"));
---
+++ 2-JavaScript/class-07-array-methods/script.js
@@ -0,0 +1,17 @@
+// map
+
+const numbers = [10, 20, 30];
+console.log(numbers.map((num) => num * 5.74));
+
+const devs = [
+  { name: "Matheus Rian", salary: 7000 },
+  { name: "John", salary: 6000 },
+  { name: "Chico", salary: 5000 },
+];
+
+const newSalaries = devs.map((dev) => dev.salary * 1.1);
+console.log(newSalaries);
+
+console.log(devs );
+
+
